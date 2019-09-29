a104f849e6098f1afbc2b1aeb064feee86ccf3e3
Thaymer Portillo <thaymerapv@gmail.com>
2019-09-29 02:56:04 -0400

    telmo al poder

--- templates/close-html.template.php
+++ templates/close-html.template.php
@@ -1,6 +1,8 @@
 <?php # JavaScript Scripts ?>
     <script src="<?php echo HOST; ?>/node_modules/jquery/dist/jquery.min.js"></script>
     <script src="<?php echo HOST; ?>/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="<?php echo HOST; ?>/node_modules/jquery-match-height/dist/jquery.matchHeight-min.js"></script>
+    <script src="<?php echo HOST; ?>/assets/js/app.js"></script>
 <?php # End of website body ?>
 </body>
 </html>
--- templates/our-services.template.php
+++ templates/our-services.template.php
@@ -1,1 +1,115 @@
-<h1>Our Services</h1>
+<div class="row">
+    <div class="col-sm-6 col-xl-3 col-md-4 col-lg-3 p-2">
+        <div data-aos="flip-left"
+             data-aos-easing="ease-out-cubic"
+             data-aos-duration="2000">    
+            <div class="card item-Height p-2">
+                <div class="c zoom ">
+                    <img class="icon" src="<?php echo HOST; ?>/assets/public/img/Choosing-a-Commercial-Cleaning-Company-in-Springfield-Missouri.jpg" alt="">
+                </div>    
+                <div class="card-tittle text-center">
+                    <h3>Commercial Cleaning</h3>
+                </div> 
+            </div>   
+        </div>
+    </div>
+    <div class="col-sm-6 col-xl-3 col-md-4 col-lg-3 p-2">
+        <div data-aos="flip-left"
+             data-aos-easing="ease-out-cubic"
+             data-aos-duration="2000">    
+            <div class="card item-Height p-2">
+                <div class="c zoom ">
+                    <img class="icon" src="<?php echo HOST; ?>/assets/public/img/download.jpeg" alt="">
+                </div>    
+                <div class="card-tittle text-center">
+                    <h3>Residential Cleaning</h3>
+                </div> 
+            </div>   
+        </div>
+    </div>
+    <div class="col-sm-6 col-xl-3 col-md-4 col-lg-3 p-2">
+        <div data-aos="flip-left"
+             data-aos-easing="ease-out-cubic"
+             data-aos-duration="2000">    
+            <div class="card item-Height p-2">
+                <div class="c zoom ">
+                    <img class="icon" src="<?php echo HOST; ?>/assets/public/img/carpet-blue-sm.jpg" alt="">
+                </div>    
+                <div class="card-tittle text-center">
+                    <h3>Carpet cleaning</h3>
+                </div> 
+            </div>   
+        </div>
+    </div>
+    <div class="col-sm-6 col-xl-3 col-md-4 col-lg-3 p-2">
+        <div data-aos="flip-left"
+             data-aos-easing="ease-out-cubic"
+             data-aos-duration="2000">    
+            <div class="card item-Height p-2">
+                <div class="c zoom ">
+                    <img class="icon" src="<?php echo HOST; ?>/assets/public/img/washing-sidewalk.jpg" alt="">
+                </div>    
+                <div class="card-tittle text-center">
+                    <h3>Pressure washing</h3>
+                </div> 
+            </div>   
+        </div>
+    </div>
+    <div class="col-sm-6 col-xl-3 col-md-4 col-lg-3 p-2">
+        <div data-aos="flip-left"
+             data-aos-easing="ease-out-cubic"
+             data-aos-duration="2000">    
+            <div class="card item-Height p-2">
+                <div class="c zoom ">
+                    <img class="icon" src="<?php echo HOST; ?>/assets/public/img/interior-painting-tips-cotton-drop-cloth.jpg" alt="">
+                </div>    
+                <div class="card-tittle text-center">
+                    <h3>Interior painting</h3>
+                </div> 
+            </div>   
+        </div>
+    </div>
+    <div class="col-sm-6 col-xl-3 col-md-4 col-lg-3 p-2">
+        <div data-aos="flip-left"
+             data-aos-easing="ease-out-cubic"
+             data-aos-duration="2000">    
+            <div class="card item-Height p-2">
+                <div class="c zoom ">
+                    <img class="icon" src="<?php echo HOST; ?>/assets/public/img/painterforaday.jpg" alt="">
+                </div>    
+                <div class="card-tittle text-center">
+                    <h3>Exterior painting</h3>
+                </div> 
+            </div>   
+        </div>
+    </div>
+    <div class="col-sm-6 col-xl-3 col-md-4 col-lg-3 p-2">
+        <div data-aos="flip-left"
+             data-aos-easing="ease-out-cubic"
+             data-aos-duration="2000">    
+            <div class="card item-Height p-2">
+                <div class="c zoom ">
+                    <img class="icon" src="<?php echo HOST; ?>/assets/public/img/commercial_painting_banner_1.jpg" alt="">
+                </div>    
+                <div class="card-tittle text-center">
+                    <h3>Comercial painting</h3>
+                </div> 
+            </div>   
+        </div>
+    </div>
+    <div class="col-sm-6 col-xl-3 col-md-6 col-lg-3 p-2">
+        <div data-aos="flip-left"
+             data-aos-easing="ease-out-cubic"
+             data-aos-duration="2000">
+            <div class="card item-Height p-2 ">
+                <div class="c zoom">
+                    <img class="icon" src="<?php echo HOST; ?>/assets/public/img/CommercialResidentialPainting2.jpg" alt="">
+                </div>
+                <div class="card-tittle text-center">
+                    <h3>Residential Painting</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
